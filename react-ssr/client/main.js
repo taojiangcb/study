a762280cbd8b19b7f7a26b89936b1f17fc21f4fc
jiangtao <jiangtao_qepyou@163.com>
2019-10-25 19:46:45 +0800

    删除失败的练习

--- react-ssr/client/main.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './main.js';
-
-ReactDOM.render(<App></App>,document.getElementById('root'));
--- react-ssr/client/server-entry.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import App from './App.jsx';
-
-
-export default <App/>
