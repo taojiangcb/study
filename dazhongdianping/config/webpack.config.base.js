280a3465761fce6fbb143552edc7f63d841d81a3
jiangtao <jiangtao_qepyou@163.com>
2019-11-04 21:33:22 +0800

    大众点评项目练习

---
+++ dazhongdianping/config/webpack.config.base.js
@@ -0,0 +1,41 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'eslint-loader',
+
+      },
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        }
+
+      },
+      { test: /\.less$/, exclude: /node_modules/, loader: 'style!css!postcss!less' },
+      { test: /\.css$/, exclude: /node_modules/, loader: 'style!css!postcss' },
+      { test: /\.(png|gif|jpg|jpeg|bmp)$/i, loader: 'url-loader?limit=5000' },  // 限制大小5kb
+      { test: /\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader: 'url-loader?limit=5000' } // 限制大小小于5k
+    ]
+  },
+
+  // eslint: {
+  //   configFile: '.eslintrc' // Rules for eslint
+  // },
+
+  plugins: [
+    require('autoprefixer'), //调用autoprefixer插件，例如 display: flex
+    // html 模板插件
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../app/index.tmpl.html'),
+    }),
+  ]
+}
---
+++ dazhongdianping/config/webpack.config.dev.js
@@ -0,0 +1,45 @@
+var path = require('path')
+var webpack = require('webpack')
+const merge = require('webpack-merge');
+const baseConf = require('./webpack.config.base');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+const webpack_dev = merge(baseConf, {
+  entry: path.resolve(__dirname, '../app/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, '../'),
+    filename: "bundle.js"
+  },
+
+  devServer: {
+    proxy: {
+      // 凡是 `/api` 开头的 http 请求，都会被代理到 localhost:3000 上，由 koa 提供 mock 数据。
+      // koa 代码在 ./mock 目录中，启动命令为 npm run mock
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false
+      }
+    },
+    contentBase: "./public", //本地服务器所加载的页面所在的目录
+    colors: true, //终端中输出结果为彩色
+    historyApiFallback: true, //不跳转
+    inline: true, //实时刷新
+    hot: true  // 使用热加载插件 HotModuleReplacementPlugin
+  },
+
+  plugins: [
+    // 热加载插件
+    new webpack.HotModuleReplacementPlugin(),
+    // 打开浏览器
+    new OpenBrowserPlugin({
+      url: 'http://localhost:8080'
+    }),
+
+    // 可在业务 js 代码中使用 __DEV__ 判断是否是dev模式（dev模式下可以提示错误、测试报告等, production模式不提示）
+    new webpack.DefinePlugin({
+      __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
+    })
+  ]
+})
+
+module.exports = webpack_dev;
